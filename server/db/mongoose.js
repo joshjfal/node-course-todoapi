7ac81ea5b8d958ea832b23826aaafac46cd7f50e
Josh Janisch <josh@janisch.co.uk>
2018-01-24 22:19:49 +0000

    Updated to mLab herokuapp address

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,8 @@
 var mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise; // Tells mongo.Promise to use the Global promise (and not a third-party promise, which is where "promise" originated before becoming part of JS)
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/TodoApp");
+mongoose.connect("mongodb://herokuapp:Hgt8khSxpqmqcU@ds113738.mlab.com:13738/herokuapp");
 
-//String from mLab: mongodb://herokuapp:Hgt8khSxpqmqcU@ds113738.mlab.com:13738/herokuapp
+// "mongodb://localhost:27017/TodoApp"
 
 module.exports = { mongoose };
