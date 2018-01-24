3371745aa703f463c38851e5f9f556be3979d897
Josh Janisch <josh@janisch.co.uk>
2018-01-24 21:37:44 +0000

    Pushing to heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,6 +3,6 @@ var mongoose = require("mongoose");
 mongoose.Promise = global.Promise; // Tells mongo.Promise to use the Global promise (and not a third-party promise, which is where "promise" originated before becoming part of JS)
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/TodoApp");
 
-//mongodb://herokuapp:Hgt8khSxpqmqcU@ds113738.mlab.com:13738/herokuapp
+//String from mLab: mongodb://herokuapp:Hgt8khSxpqmqcU@ds113738.mlab.com:13738/herokuapp
 
 module.exports = { mongoose };
